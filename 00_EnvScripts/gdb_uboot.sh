c16c4357cfdb9d7cefc4ef64bd1b9b7b83fb98b1
ywy0318 <825579631@qq.com>
2026-06-12 23:45:40 +0800

    auto sync top-level shell scripts, no deep subdir 2026-06-12 23:45:40

--- 00_EnvScripts/gdb_uboot.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# gdb_uboot.sh
-
-UBOOT_DIR=./u-boot-2023.04
-GDB=aarch64-linux-gnu-gdb
-
-cd $UBOOT_DIR || { echo "cd $UBOOT_DIR failed"; exit 1; }
-
-$GDB u-boot -ex "target remote :1234"
